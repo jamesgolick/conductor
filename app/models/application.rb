2932c24d28b1af9860a0efa03197ce48c77d0aac
James Golick <james@giraffesoft.ca>
2009-06-13 12:27:50 -0400

    setup application has many environments

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,4 @@
 class Application < ActiveRecord::Base
+  has_many :environments
   validates_presence_of :name, :clone_url
 end
--- test/unit/application_test.rb
+++ test/unit/application_test.rb
@@ -2,4 +2,5 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ApplicationTest < Test::Unit::TestCase
   should_validate_presence_of :name, :clone_url
+  should_have_many :environments
 end
