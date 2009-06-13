86b4694da09f6c55d56cc195b6469d55753f7184
James Golick <james@giraffesoft.ca>
2009-06-13 13:08:18 -0400

    environment has many instances

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,4 +1,5 @@
 class Environment < ActiveRecord::Base
   belongs_to :application
+  has_many   :instances
   validates_presence_of :name
 end
--- test/unit/environment_test.rb
+++ test/unit/environment_test.rb
@@ -2,5 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class EnvironmentTest < Test::Unit::TestCase
   should_belong_to :application
+  should_have_many :instances
   should_validate_presence_of :name
 end
