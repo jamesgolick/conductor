f263f8258cba27b34037aba9407eff4b3d22d843
James Golick <james@giraffesoft.ca>
2009-06-13 13:18:34 -0400

    setup enum fields for roles, zones, etc

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,7 +1,7 @@
 class Instance < ActiveRecord::Base
   belongs_to :environment
 
-  validates_presence_of :size
-  validates_presence_of :role
-  validates_presence_of :zone
+  enum_field :size, %w( m1.small m1.large m1.xlarge c1.medium c1.xlarge )
+  enum_field :role, %w( mysql-master app-server )
+  enum_field :zone, %w( us-east-1a us-east-1b us-east-1c us-east-1d )
 end
--- test/unit/instance_test.rb
+++ test/unit/instance_test.rb
@@ -2,5 +2,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class InstanceTest < Test::Unit::TestCase
   should_belong_to :environment
-  should_validate_presence_of :size, :zone, :role
+  should_allow_values_for :size, Instance::SIZES
+  should_not_allow_values_for :size, %w(other stuff), :message => /invalid size/i
+  should_allow_values_for :role, Instance::ROLES
+  should_not_allow_values_for :role, %w(other stuff), :message => /invalid role/i
+  should_allow_values_for :zone, Instance::ZONES
+  should_not_allow_values_for :zone, %w(us-east-1z), :message => /invalid zone/i
 end
