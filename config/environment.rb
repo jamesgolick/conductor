c269ccdb5e9a6ef27d0d4eec593134cfc27508c3
James Golick <james@giraffesoft.ca>
2009-06-13 16:18:14 -0400

    add aws gem

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-factory_girl",         :lib => "factory_girl"
   config.gem "authlogic"
   config.gem "cucumber"
+  config.gem "aws",                             :lib => "right_aws"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
