7002de79a03b7df5ee6abe2d0f1c0eb8930d2ef0
James Golick <james@giraffesoft.ca>
2009-06-15 11:52:29 -0400

    make a bootstrapper that just calls out to a shell script.. all this other shit was too hard

---
+++ app/models/bootstrapper.rb
@@ -0,0 +1,23 @@
+require 'fileutils'
+
+class Bootstrapper
+  attr_reader :instance
+
+  def initialize(instance)
+    @instance = instance
+  end
+
+  def bootstrap
+    `#{cmd}`
+  end
+
+  protected
+    def cmd
+      "#{script_location} #{instance.application.name} #{instance.application.cookbook_clone_url} #{instance.dns_name}"
+    end
+
+    def script_location
+      Rails.root + "/script/bootstrap"
+    end
+end
+
