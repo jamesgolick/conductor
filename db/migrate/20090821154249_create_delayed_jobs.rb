8e9575bfee5be45698167f2b68b48e3be2b0958b
James Golick <james@giraffesoft.ca>
2009-08-21 11:43:47 -0400

    create delayed jobs table

---
+++ db/migrate/20090821154249_create_delayed_jobs.rb
@@ -0,0 +1,20 @@
+class CreateDelayedJobs < ActiveRecord::Migration
+  def self.up
+    create_table :delayed_jobs, :force => true do |table|
+      table.integer  :priority, :default => 0
+      table.integer  :attempts, :default => 0
+      table.text     :handler
+      table.string   :last_error
+      table.datetime :run_at
+      table.datetime :locked_at
+      table.datetime :failed_at
+      table.string   :locked_by
+      table.timestamps
+    end
+
+  end
+
+  def self.down
+    drop_table :delayed_jobs
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090703200434) do
+ActiveRecord::Schema.define(:version => 20090821154249) do
 
   create_table "applications", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,19 @@ ActiveRecord::Schema.define(:version => 20090703200434) do
     t.integer  "instance_id"
     t.boolean  "successful"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.string   "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
