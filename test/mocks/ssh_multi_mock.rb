4edf6c471f7119acb558ec44000ce16021ba5704
James Golick <james@giraffesoft.ca>
2009-08-21 23:59:18 -0400

    add the exec method to the ssh multi mock

--- test/mocks/ssh_multi_mock.rb
+++ test/mocks/ssh_multi_mock.rb
@@ -1,9 +1,24 @@
 class SSHMultiMock
-  attr_reader :servers_used
+  attr_reader :servers_used, :responses
+
+  def initialize
+    @servers_used = []
+    @responses    = {}
+  end
 
   def use(host)
-    @servers_used ||= []
     @servers_used << host
+  end
+
+  def add_command_response(command, *response)
+    responses[command] ||= []
+    responses[command] << response
+  end
+
+  def exec(command, &block)
+    responses[command].each do |response|
+      block.call(*response)
+    end
   end
 end
 
--- test/unit/mocks/ssh_multi_mock_test.rb
+++ test/unit/mocks/ssh_multi_mock_test.rb
@@ -2,14 +2,33 @@ require File.expand_path('../../../test_helper', __FILE__)
 require 'test/mocks/ssh_multi_mock'
 
 class SSHMultiMockTestTest < ActiveSupport::TestCase
+  def setup
+    @ssh = SSHMultiMock.new
+  end
+
   context "Using a server" do
     setup do
-      @ssh = SSHMultiMock.new
       @ssh.use "james@myserver.com"
     end
 
     should "record the servers that are in use" do
       assert_equal ["james@myserver.com"], @ssh.servers_used
+    end
+  end
+
+  context "Running a command with configured yields" do
+    setup do
+      @ssh.add_command_response "ls -la", {:host => "whatever"}, :stdout, "the stdout data"
+      @ssh.add_command_response "ls -la", {:host => "whatever"}, :stderr, "the stderr data"
+    end
+
+    should "yield them to the block passed to exec in order" do
+      calls = []
+      block_stub = lambda { |*response| calls << response }
+      @ssh.exec("ls -la", &block_stub)
+
+      assert_equal [{:host => "whatever"}, :stdout, "the stdout data"], calls.first
+      assert_equal [{:host => "whatever"}, :stderr, "the stderr data"], calls[1]
     end
   end
 end
