f8a44e95c6584bccac532dc0be800a807642b9a0
James Golick <james@giraffesoft.ca>
2009-08-23 10:52:53 -0400

    change the little script that bootstraps the deployment

--- app/models/bootstrap_deployment.rb
+++ app/models/bootstrap_deployment.rb
@@ -1,9 +1,17 @@
 class BootstrapDeployment < Deployment
   class << self
     def command
-      "apt-get install git-core && 
-        git clone git@github.com/FetLife/fetlife-systems.git /var/chef &&
-          /var/chef/bootstrap/bootstrap.sh"
+      "apt-get update &&
+        apt-get install -y git-core && 
+      echo '|1|sv3Od6rY/siudwEpT+g2xn7YnsU=|8Rtg7HTzZS0UmR2NHzALVgQdn2A= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+            |1|bCSVbXPailzmrTScHcmJkv56zxI=|zJu/QV7pkexhJorVEzrNXnY9UBk= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts && 
+          if [ -d /var/chef ]; then
+            cd /var/chef/bootstrap && git pull
+          else
+            git clone git@github.com:giraffesoft/conductor-cookbooks.git /var/chef
+          fi &&
+             /var/chef/bootstrap/bootstrap.sh
+            "
     end
   end
 
