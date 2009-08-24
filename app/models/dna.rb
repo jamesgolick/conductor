7ac6d3bfe62a01fa8620164f558aaf60952087f0
James Golick <james@giraffesoft.ca>
2009-08-24 09:28:48 -0400

    have the dna read the attributes.rb file out of the cookbook

--- app/models/dna.rb
+++ app/models/dna.rb
@@ -1,11 +1,26 @@
 class Dna < HashWithIndifferentAccess
-  def initialize(role)
-    super
-    self[:run_list] = ["roles[#{role}]"]
+  attr_reader :cookbook, :role
+
+  def initialize(role, cookbook)
+    super()
+    @role     = role
+    @cookbook = cookbook
+
+    set_initial_run_list
+    eval_attributes_file
   end
 
   def method_missing(name, *args, &block)
     args.empty? ? self[name] : self[name] = args.first
   end
+
+  protected
+    def set_initial_run_list
+      self[:run_list] = ["roles[#{role}]"]
+    end
+
+    def eval_attributes_file
+      instance_eval(cookbook.read("attributes.rb"), "attributes.rb")
+    end
 end
 
--- test/unit/dna_test.rb
+++ test/unit/dna_test.rb
@@ -3,17 +3,24 @@ require File.expand_path('../../test_helper', __FILE__)
 class DnaTest < ActiveSupport::TestCase
   context "initializing the dna for an instance type" do
     setup do
-      @dna = Dna.new("app")
+      CookbookRepository.any_instance.stubs(:clone).stubs(:pull)
+      @repo = CookbookRepository.new("doesn't matter")
+      @repo.stubs(:read).returns("some_attr 'a value'")
+      @dna  = Dna.new("app", @repo)
     end
 
     should "automatically add that to the runlist" do
       assert_equal ["roles[app]"], @dna[:run_list]
     end
+
+    should "instance eval the attributes file" do
+      assert_equal "a value", @dna.some_attr
+    end
   end
 
   context "Accessing hash keys via method syntax" do
     setup do
-      @dna = Dna.new("app")
+      @dna = Dna.new("app", stub_everything(:read => ''))
       @dna.some_attribute "some value"
     end
 
