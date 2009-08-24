38cc9247809662a8dd0206d36e5ae6ea081bcfc6
James Golick <james@giraffesoft.ca>
2009-08-24 14:13:41 -0400

    make it possible to serialize the environment's instances

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,6 +1,16 @@
 class Environment < ActiveRecord::Base
   belongs_to :application
-  has_many   :instances
+
+  has_many   :instances do
+    def to_dna
+      running.inject({}) do |memo, instance|
+        memo[instance.role.to_sym] ||= []
+        memo[instance.role.to_sym] << instance.private_dns_name
+        memo
+      end
+    end
+  end
+
   validates_presence_of :name
 
   def has_database_server?
--- test/unit/environment_test.rb
+++ test/unit/environment_test.rb
@@ -5,6 +5,10 @@ class EnvironmentTest < Test::Unit::TestCase
   should_have_many :instances
   should_validate_presence_of :name
 
+  def setup
+    Ec2.mode = :test
+  end
+
   context "When a database server has been created" do
     setup do
       @instance = Factory(:mysql_master)
@@ -22,6 +26,30 @@ class EnvironmentTest < Test::Unit::TestCase
 
     should "not be has_database_server?" do
       assert !@environment.has_database_server?
+    end
+  end
+
+  context "Getting the instances as a hash" do
+    setup do
+      @environment = Factory(:environment)
+      @db  = Factory(:mysql_master, :environment      => @environment,
+                                    :private_dns_name => "mysql_master.local")
+      @db.update_attribute :aws_state, "running"
+
+      @app = Factory(:app_server,   :environment      => @environment,
+                                    :private_dns_name => "app.local")
+      @app.update_attribute :aws_state, "running"
+      
+      Factory(:app_server,   :environment      => @environment,
+                             :private_dns_name => "app.local")
+    end
+
+    should "create a 2-level hash with the running servers' privatedns names" do
+      expected = {
+        :app          => ["app.local"],
+        :mysql_master => ["mysql_master.local"]
+      }
+      assert_equal expected, @environment.instances.to_dna
     end
   end
 end
