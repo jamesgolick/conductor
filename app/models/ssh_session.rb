cd9c4866473b3b028525b640f2d2c274819bfc95
James Golick <james@giraffesoft.ca>
2009-09-14 22:22:26 -0400

    start rewroking ssh session to actually be multi

--- app/models/ssh_session.rb
+++ app/models/ssh_session.rb
@@ -2,12 +2,15 @@ require 'net/ssh'
 require 'net/sftp'
 
 class SshSession
-  attr_reader :ssh, :host
+  attr_reader :ssh, :hosts
 
-  def initialize(host)
-    @ssh  = Net::SSH::Multi.start
-    @host = host
-    ssh.use(host, :forward_agent => true)
+  def initialize(*hosts)
+    @ssh   = Net::SSH::Multi.start
+    @hosts = hosts
+
+    hosts.each do |h|
+      ssh.use(h, :forward_agent => true)
+    end
   end
 
   def run(command)
@@ -21,33 +24,9 @@ class SshSession
       log << line
     end
     ssh.loop
-
-    CommandResult.new(host, log, channel[:exit_status])
-  end
-
-  def upload(file)
-    sftp.upload!(file, file)
-  end
-
-  def put(value, location)
-    sftp.file.open(location, "w") do |f|
-      f.puts value
-    end
   end
 
   protected
-    def sftp
-      @sftp ||= Net::SFTP.start(hostname, username)
-    end
-
-    def hostname
-      host.split("@").last
-    end
-
-    def username
-      host.split("@").first
-    end
-
     def build_line(stream, log, data)
       returning("") do |line|
         line << "[#{stream.to_s.upcase}]: " if log.ends_with?("\n") || log.blank?
--- test/unit/ssh_session_test.rb
+++ test/unit/ssh_session_test.rb
@@ -6,7 +6,9 @@ class SshSessionTest < Test::Unit::TestCase
     should "tell the ssh session to use all of the hosts" do
       Net::SSH::Multi::Session.any_instance.
         expects(:use).with("james@myserver.com", :forward_agent => true)
-      SshSession.new("james@myserver.com")
+      Net::SSH::Multi::Session.any_instance.
+        expects(:use).with("james@myotherserver.com", :forward_agent => true)
+      SshSession.new("james@myserver.com", "james@myotherserver.com")
     end
   end
 
@@ -21,7 +23,6 @@ class SshSessionTest < Test::Unit::TestCase
       @session.stubs(:ssh).returns(@multi_mock)
 
       @result    = @session.run("ls -la")
-      @log_lines = @result.log.split("\n")
     end
 
     should "yield the log lines to the supplied block" do
@@ -32,52 +33,6 @@ class SshSessionTest < Test::Unit::TestCase
       assert_equal "[STDOUT]: stdout", yields.first
       assert_equal " output\n", yields[1]
       assert_equal "[STDERR]: stderr output\n", yields[2]
-    end
-
-    should "return the log of the command" do
-      assert_equal "[STDOUT]: stdout output", @log_lines.first
-      assert_equal "[STDERR]: stderr output", @log_lines[1]
-    end
-
-    should "return the exit code" do
-      assert_equal 0, @result.exit_code
-    end
-
-    should "return the host" do
-      assert_equal "james@myserver.com", @result.host
-    end
-  end
-
-  context "Running an SFTP command" do
-    setup do
-      @session = SshSession.new("james@myserver.com")
-    end
-
-    should "split the user from the host before passing it to net/sftp" do
-      Net::SFTP.expects(:start).with("myserver.com", "james").returns(stub_everything)
-      @session.upload("some_file")
-    end
-
-    should "upload the file immediately via sftp" do
-      session_mock = mock
-      session_mock.expects(:upload!).with("somefile", "somefile")
-      Net::SFTP.stubs(:start).returns(session_mock)
-
-      @session.upload("somefile")
-    end
-  end
-
-  context "Putting a string via sftp" do
-    should "put the file using net/sftp" do
-      @session = SshSession.new("james@myserver.com")
-      open_file_stub = stub
-      open_file_stub.expects(:puts).with("the value")
-      file_stub      = stub
-      file_stub.expects(:open).with("/etc/chef/dna.json", "w").yields(open_file_stub)
-      sftp_stub      = stub
-      sftp_stub.stubs(:file).returns(file_stub)
-      @session.stubs(:sftp).returns(sftp_stub)
-      @session.put("the value", "/etc/chef/dna.json")
     end
   end
 end
