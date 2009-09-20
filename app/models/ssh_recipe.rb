9e44e242c299c3944285f5ee93a03138ad757922
James Golick <james@giraffesoft.ca>
2009-09-20 12:45:22 -0400

    when running a recipe, if a command fails, don't run any more commands, and return the failing result proxy

--- app/models/ssh_recipe.rb
+++ app/models/ssh_recipe.rb
@@ -13,5 +13,12 @@ class SshRecipe
   def put(*args)
     @commands << [:put, *args]
   end
+
+  def exec(session)
+    commands.each do |c|
+      result = session.send(c.shift, *c)
+      return result unless result.successful?
+    end
+  end
 end
 
--- test/unit/ssh_recipe_test.rb
+++ test/unit/ssh_recipe_test.rb
@@ -16,4 +16,32 @@ class SshRecipeTest < ActiveSupport::TestCase
       assert_equal [:run, "other stuffs"], @recipe.commands[2]
     end
   end
+
+  context "Running a recipe" do
+    setup do
+      @recipe  = SshRecipe.new do
+        run "ls -la"
+        run "rm -Rf /"
+      end
+      @session = SshSession.new
+    end
+
+    should "run it on the supplied session" do
+      @session.expects(:run).with("ls -la").returns(stub(:successful? => true))
+      @session.expects(:run).with("rm -Rf /").returns(stub(:successful? => true))
+      @recipe.exec(@session)
+    end
+
+    context "when a command fails" do
+      setup do
+        @proxy = SshSession::ResultProxy.new([stub(:successful? => false)])
+        @session.stubs(:run).with("ls -la").returns(@proxy)
+        @session.expects(:run).with("rm -Rf /").never
+      end
+
+      should "stop running the commands and return the failing resultproxy" do
+        assert_equal @proxy, @recipe.exec(@session)
+      end
+    end
+  end
 end
