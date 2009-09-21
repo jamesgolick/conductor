6118659693987c887364f24143a3953f0ca5f2ad
James Golick <james@giraffesoft.ca>
2009-09-21 12:58:12 -0400

    spike for roughly how the deployment runner will perform a deployment

--- app/models/deployment_runner.rb
+++ app/models/deployment_runner.rb
@@ -1,12 +1,22 @@
 class DeploymentRunner
-  attr_reader :instances, :logs
+  attr_reader :instances, :logger
 
   def initialize(*instances)
     @instances = instances
+    @logger    = DeploymentLogger.new(:chef, *instances)
   end
 
   def perform_deployment
-    build_logs
+    set_instance_states(:deploying)
+    result = @session = SshSession.new(*instances.map(&:connection_string)) do
+      put build_put_command
+      run "cd /var/chef && git pull"
+      run "/usr/bin/chef-solo -j /etc/chef/dna.json"
+      before_command { |command| logger.system_message("Running command #{command}.") }
+      on_data { |host, stream, data| logger.log(host, stream, data) }
+    end.execute
+
+    result.successful? ? log_success : log_failure(result)
   end
 
   protected
@@ -15,6 +25,10 @@ class DeploymentRunner
         hash[instance] = instance.chef_logs.create
         hash
       end
+    end
+
+    def set_instance_states(state)
+      instances.each { |i| i.update_attributes :state => state.to_s }
     end
 end
 
