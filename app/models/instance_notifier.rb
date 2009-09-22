e3e5f25f6885e21e2a20970eaad389071cb70383
James Golick <james@giraffesoft.ca>
2009-09-22 12:47:21 -0400

    notify of cancellation and failure

--- app/models/instance_notifier.rb
+++ app/models/instance_notifier.rb
@@ -14,9 +14,24 @@ class InstanceNotifier
     notify_all :start
   end
 
+  def cancelled(failed_hosts)
+    notify_some(failed_hosts, :failure, :cancelled)
+  end
+
+  def failure(failed_hosts)
+    notify_some(failed_hosts, :failure, :successful)
+  end
+
   protected
     def notify_all(event)
       instances.each { |i| i.deployment_event(runner, event) }
+    end
+
+    def notify_some(hosts, included, not_included)
+      instances.each do |i|
+        event = hosts.include?(i.dns_name) ? included : not_included
+        i.deployment_event(runner, event)
+      end
     end
 end
 
--- test/unit/instance_notifier_test.rb
+++ test/unit/instance_notifier_test.rb
@@ -2,26 +2,33 @@ require 'test_helper'
 
 class InstanceNotifierTest < ActiveSupport::TestCase
   def setup
-    Ec2.mode = :test
+    Ec2.mode  = :test
+    @success  = Factory(:running_instance)
+    @failure  = Factory(:running_instance, :dns_name => "failsauce.com")
+    @deployer = ChefDeploymentRunner.new
+    @notifier = InstanceNotifier.new(@deployer, @success, @failure)
   end
 
-  context "Notifying a set of instances" do
-    setup do
-      @success  = Factory(:running_instance)
-      @failure  = Factory(:running_instance, :dns_name => "failsauce.com")
-      @deployer = ChefDeploymentRunner.new
-      @notifier = InstanceNotifier.new(@deployer, @success, @failure)
-    end
+  should "notify of start" do
+    expect_all(:start)
+    @notifier.start
+  end
 
-    should "notify of start" do
-      expect_all(:start)
-      @notifier.start
-    end
+  should "notify of success" do
+    expect_all(:successful)
+    @notifier.successful
+  end
 
-    should "notify of success" do
-      expect_all(:successful)
-      @notifier.successful
-    end
+  should "notify of cancellation" do
+    @failure.expects(:deployment_event).with(@deployer, :failure)
+    @success.expects(:deployment_event).with(@deployer, :cancelled)
+    @notifier.cancelled(["failsauce.com"])
+  end
+
+  should "notify of failure" do
+    @failure.expects(:deployment_event).with(@deployer, :failure)
+    @success.expects(:deployment_event).with(@deployer, :successful)
+    @notifier.failure(["failsauce.com"])
   end
 
   protected
