1b12a88fd7699ad55afa1d962ea28aea4ace269b
James Golick <james@giraffesoft.ca>
2009-09-22 13:01:50 -0400

    use hte notifier in the deployment. this makes testing so much easier

--- app/models/deployment_runner.rb
+++ app/models/deployment_runner.rb
@@ -1,14 +1,15 @@
 class DeploymentRunner
-  attr_reader :instances, :logger
+  attr_reader :instances, :logger, :notifier
 
   def initialize(*instances)
     @instances = instances
     @logger    = DeploymentLogger.new(deployment_type, *instances)
+    @notifier  = InstanceNotifier.new(self, *instances)
     send_later :perform_deployment
   end
 
   def perform_deployment
-    notify_instances(:start)
+    notifier.start
     handle_result ssh_session.execute
   end
 
@@ -21,29 +22,19 @@ class DeploymentRunner
       raise NotImplementedError, "Subclasses must implement #deployment_type"
     end
 
-    def notify_instances(event)
-      instances.each { |i| i.deployment_event(self, event) }
-    end
-
     def handle_result(result)
       result.successful? ? handle_success(result) : handle_failure(result)
     end
 
     def handle_success(result)
       logger.system_message "Deployment ran successfully."
-      notify_instances :successful
+      notifier.successful
     end
 
     def handle_failure(result)
-      failed_hosts = result.failed_hosts.join(', ')
-
-      if result.cancelled?
-        logger.system_message "Deployment was cancelled because a command failed on [#{failed_hosts}]"
-        notify_instances :cancelled
-      else
-        logger.system_message "Deployment failed on [#{failed_hosts}]."
-        notify_instances :failure
-      end
+      logger.system_message failure_message(result)
+      event = result.cancelled? ? :cancelled : :failure
+      notifier.send(event, result.failed_hosts)
     end
 
     def create_ssh_session
@@ -55,6 +46,12 @@ class DeploymentRunner
 
     def build_ssh_session
       raise NotImplementedError, "Implement #build_ssh_session in subclasses."
+    end
+
+    def failure_message(result)
+      verb  = result.cancelled? ? "was cancelled" : "failed"
+      hosts = result.failed_hosts.join(', ')
+      "The deployment #{verb} because a command failed on [#{hosts}]."
     end
 end
 
--- test/unit/deployment_runner_test.rb
+++ test/unit/deployment_runner_test.rb
@@ -2,50 +2,47 @@ require 'test_helper'
 
 class DeploymentRunnerTest < ActiveSupport::TestCase
   def setup
-    Ec2.mode = :test
     DeploymentRunner.any_instance.stubs(:deployment_type).returns(:chef)
-    @instance = Factory(:mysql_master)
-    @runner   = DeploymentRunner.new(@instance)
+    Ec2.mode    = :test
+    @successful = Factory(:running_instance)
+    @failure    = Factory(:running_instance, :dns_name => "failsauce.com")
+    @runner     = DeploymentRunner.new(@successful, @failure)
+    @runner.stubs(:build_ssh_session).returns(SshSession.new {})
   end
 
-  context "Creating a deployment on one instance" do
-    should "create a deployment logger" do
-      assert_equal [@instance], @runner.logger.instances
-      assert_equal :chef, @runner.logger.log_type
-    end
+  should "create a deployment logger" do
+    assert_equal @runner.instances, @runner.logger.instances
+    assert_equal :chef, @runner.logger.log_type
   end
 
-  context "Running a deployment" do
-    setup do
-      @runner.stubs(:build_ssh_session).returns(SshSession.new {})
-    end
+  should "create an instance notifier" do
+    assert_equal @runner.instances, @runner.notifier.instances
+    assert_equal @runner, @runner.notifier.runner
+  end
 
-    should "run the deployment" do
-      @instance.expects(:deployment_event).with(@runner, :start)
-      @runner.ssh_session.expects(:execute).returns(mock(:successful? => true))
-      @instance.expects(:deployment_event).with(@runner, :successful)
-      @runner.perform_deployment
-    end
+  should "run the deployment" do
+    @runner.notifier.expects(:start)
+    @runner.notifier.expects(:successful)
+    @runner.ssh_session.expects(:execute).returns(mock(:successful? => true))
+    @runner.perform_deployment
+  end
 
-    should "notify the instances if one fails" do
-      @instance.expects(:deployment_event).with(@runner, :start)
-      proxy_mock = stub(:cancelled?   => false, 
-                        :successful?  => false,
-                        :failed_hosts => ["failsauce.com"])
-      @runner.ssh_session.expects(:execute).returns(proxy_mock)
-      @instance.expects(:deployment_event).with(@runner, :failure)
-      @runner.perform_deployment
-    end
+  should "notify the instances if one fails" do
+    proxy_mock = stub(:cancelled?   => false, 
+                      :successful?  => false,
+                      :failed_hosts => ["failsauce.com"])
+    @runner.notifier.expects(:failure).with(["failsauce.com"])
+    @runner.ssh_session.expects(:execute).returns(proxy_mock)
+    @runner.perform_deployment
+  end
 
-    should "notify the instance if the deployment is cancelled" do
-      @instance.expects(:deployment_event).with(@runner, :start)
-      proxy_mock = stub(:cancelled?   => true, 
-                        :successful?  => false,
-                        :failed_hosts => ["whatever.com"])
-      @runner.ssh_session.expects(:execute).returns(proxy_mock)
-      @instance.expects(:deployment_event).with(@runner, :cancelled)
-      @runner.perform_deployment
-    end
+  should "notify the instances if the deployment is cancelled" do
+    proxy_mock = stub(:cancelled?   => true, 
+                      :successful?  => false,
+                      :failed_hosts => ["failsauce.com"])
+    @runner.ssh_session.expects(:execute).returns(proxy_mock)
+    @runner.notifier.expects(:cancelled).with(["failsauce.com"])
+    @runner.perform_deployment
   end
 
   context "The ssh session" do
