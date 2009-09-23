3e34ca9a30eeb5659d81928a8df04a7dd0a3be01
James Golick <james@giraffesoft.ca>
2009-09-22 20:29:55 -0400

    remove all the deployment crap from the logs

--- app/models/bootstrap_log.rb
+++ app/models/bootstrap_log.rb
@@ -1,26 +1,3 @@
 class BootstrapLog < Log
-  self.command = 
-      "apt-get update &&
-        apt-get install -y git-core && 
-      echo '|1|sv3Od6rY/siudwEpT+g2xn7YnsU=|8Rtg7HTzZS0UmR2NHzALVgQdn2A= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-            |1|bCSVbXPailzmrTScHcmJkv56zxI=|zJu/QV7pkexhJorVEzrNXnY9UBk= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts && 
-          if [ -d /var/chef ]; then
-            cd /var/chef/bootstrap && git pull
-          else
-            git clone git@github.com:giraffesoft/conductor-cookbooks.git /var/chef
-          fi &&
-             /var/chef/bootstrap/bootstrap.sh "
-
-  protected
-    def notify_instance_of_success
-      instance.bootstrapped!
-    end
-
-    def notify_instance_of_start
-      instance.bootstrapping!
-    end
-
-    def notify_instance_of_failure
-    end
 end
 
--- test/unit/bootstrap_log_test.rb
+++ test/unit/bootstrap_log_test.rb
@@ -1,19 +1,4 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class BootstrapLogTest < ActiveSupport::TestCase
-  def setup
-    Ec2.mode = :test
-    @instance   = Factory(:instance, :role => "mysql_master")
-    @deployment = BootstrapLog.new :instance => @instance
-  end 
-
-  should "notify the instance of start" do
-    @instance.expects(:bootstrapping!)
-    @deployment.send(:notify_instance_of_start)
-  end
-
-  should "notify the instance of success" do
-    @instance.expects(:bootstrapped!)
-    @deployment.send(:notify_instance_of_success)
-  end
 end
