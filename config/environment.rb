923fb691778dcf20e87ad461aedcfdfec5552594
James Golick <james@giraffesoft.ca>
2009-09-23 08:28:49 -0600

    switch to jferris-mocha

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "net-ssh-multi",                   :lib => "net/ssh/multi"
   config.gem "tobi-delayed_job",                :lib => "delayed_job"
   config.gem "andand"
+  config.gem "jferris-mocha",                   :lib => "mocha"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
