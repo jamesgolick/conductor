2211deaaead2cf6a2b601faf8c36fc2f602b678c
James Golick <james@giraffesoft.ca>
2009-09-23 08:04:36 -0600

    the log should be responsible for appending its own data

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,8 @@
 class Log < ActiveRecord::Base
+  def append(data)
+    update_attribute :log, [log, data].join
+  end
+
   def last_line
     log.split("\n").last
   end
--- test/unit/log_test.rb
+++ test/unit/log_test.rb
@@ -1,4 +1,16 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class LogTest < ActiveSupport::TestCase
+  context "Appending data to a log" do
+    setup do
+      @log = Log.new :log => "something, something else\n"
+      @log.append "and some other stuff"
+    end
+
+    should "save the appended data to the log" do
+      expected =  "something, something else\nand some other stuff"
+      assert_equal expected, @log.reload.log
+    end
+  end
 end
+
