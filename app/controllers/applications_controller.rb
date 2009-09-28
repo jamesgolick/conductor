86731072a13053d344f541ad0b302f5082dc49b6
James Golick <james@giraffesoft.ca>
2009-09-27 21:43:39 -0700

    redirect to environments url, because that makes more sense

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationsController < ResourceController::Base
   create.flash "Your application has been created."
+  create.wants.html { redirect_to application_environments_url(object) }
+  update.wants.html { redirect_to application_environments_url(object) }
 end
